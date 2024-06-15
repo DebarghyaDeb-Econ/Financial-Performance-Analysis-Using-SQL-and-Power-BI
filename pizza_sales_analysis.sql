5744b0127c67ef45874e8f0a82cf81a1db4a668a
DebarghyaDeb-Econ <debarghyadeb02@gmail.com>
2024-06-15 20:26:49 +0530

    Pizza_Sales_Analysis.sql

--- pizza_sales_analysis.sql
+++ pizza_sales_analysis.sql
@@ -1,10 +1,5 @@
--- create a database for the pizza dataset
-CREATE DATABASE IF NOT EXISTS Pizza_sales;
 
--- import your dataset into your database or sql server through import wizard
-SELECT * FROM Pizza_sales.dbo.pizza_sales;
-
--- KPI's
+ I. KEY PERFORMANCE INDICATORS
 
 -- total revenue
 SELECT SUM(total_price) AS Total_Revenue 
@@ -27,30 +22,30 @@ SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2))/CAST(COUNT(DISTINCT order_id) A
 AS Avg_Pizzas_per_order
 FROM pizza_sales;
 
--- daily trend for total orders
+II. Daily trend for total orders
 SELECT DATENAME(DW, order_date) AS order_day, COUNT(DISTINCT order_id) AS total_orders 
 FROM pizza_sales
 GROUP BY DATENAME(DW, order_date);
 
--- monthly trend for the orders
+III. Monthly trend for the orders
 select DATENAME(MONTH, order_date) as Month_Name, COUNT(DISTINCT order_id) as Total_Orders
 from pizza_sales
 GROUP BY DATENAME(MONTH, order_date);
 
--- % of sales per pizza category
-SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
-CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+IV. Percentage of sales per pizza category
+SELECT pizza_category, SUM(total_price) AS total_revenue,
+SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS PCT
 FROM pizza_sales
 GROUP BY pizza_category;
 
--- % sales per pizza size
-SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
-CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+V. Percentage of sales per pizza size
+SELECT pizza_size, SUM(total_price)  as total_revenue,
+SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS PCT
 FROM pizza_sales
 GROUP BY pizza_size
 ORDER BY pizza_size;
 
--- total pizzas sold by pizza category
+VI. Total pizzas sold by pizza category
 SELECT pizza_category, SUM(quantity) as Total_Quantity_Sold
 FROM pizza_sales
 WHERE MONTH(order_date) = 2
@@ -58,7 +53,8 @@ GROUP BY pizza_category
 ORDER BY Total_Quantity_Sold DESC;
 
 
--- Best/Worst performing pizzas
+VII. Best/Worst performing pizzas
+
 -- top 5 pizza by revenue
 SELECT Top 5 pizza_name, SUM(total_price) AS Total_Revenue
 FROM pizza_sales
